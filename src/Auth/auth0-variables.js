4f62154b227250cab33eca506814e7041f6de3b3
Trinadh Maddali <tmaddali@sapient.com>
2018-09-09 15:37:39 +0530

    Updated proxy and callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'my-play-ground.auth0.com',
   clientId: 'iArHvZ-hqEd8-5hVmnhWzv0F5XGRSmJy',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: '/callback'
 }
